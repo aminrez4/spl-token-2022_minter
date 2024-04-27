1b585fa8fa7f85cdb47b9bbfc10bc42e8459bfac
aminrez4 <61351266+aminrez4@users.noreply.github.com>
2024-04-27 17:08:57 +0200

    fixed tranfer at a pda

--- src/CreateAndMint.ts
+++ src/CreateAndMint.ts
@@ -26,6 +26,9 @@ import {
   withdrawWithheldTokensFromMint,
   getOrCreateAssociatedTokenAccount,
   createAssociatedTokenAccountIdempotent,
+  getAssociatedTokenAddress ,
+  setAuthority,
+  AuthorityType,
 } from "@solana/spl-token";
 import base58 from "bs58";
 import dotenv from "dotenv";
@@ -33,13 +36,18 @@ dotenv.config();
 
 // https://www.quicknode.com/guides/solana-development/spl-tokens/token-2022/transfer-fees
 // Initialize connection to local Solana node
-// CHANGE PASSING RPC FRON ENV
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+const connection = new Connection("https://api.devnet.solana.com", "confirmed"); // CHANGE PASSING RPC FRON ENV
+const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
 
 
 //--------- DEFINE THE OWNER WALLET AND THE OTHERS WALLET INVOLVED -------------------------------------------------------------------------
+//DEFINE THE LOTTERY WALLET AND THE PROGRAM ID
+// const publicKeyString = "9c5cGZEwQFJKhucTYxrQjkdkXjjsKBV5q7zd2qHvGyQa"; 
+const program_Id = new PublicKey(process.env.PROGRAM_ID);// lottery Account
+const pda_lottery = new PublicKey(process.env.LOTTERY_ACCOUNT);
 // Generate keys for payer, mint authority, and mint our PRIVATE_KEY
 const payer = Keypair.fromSecretKey(base58.decode(process.env.PRIVATE_KEY));
+
 
 const mintAuthority = payer; // THE MINT AUTHORITY
 const mintOwn = payer.publicKey;
@@ -65,9 +73,8 @@ const mintLen = getMintLen(extensions);
 // Set the decimals, fee basis points, and maximum fee
 const decimals = 9;
 const feeBasisPoints = 100* 30; // 10%
-const maxFee = BigInt(100000 * Math.pow(10, decimals)); // 1000 tokens
+const maxFee = BigInt(10000000 * Math.pow(10, decimals)); // 1000 tokens
 // aggiungere max fee transfer
-
 
 // Define the amount to be minted and the amount to be transferred, accounting for decimals
 const mintAmount = BigInt(1_000_000 * Math.pow(10, decimals)); // Mint 1,000,000 tokens
@@ -90,11 +97,9 @@ async function checkTokenBalance(connection, feeVault) {
       feeVault
     );
 
-   
-    console.log("Token balance in ui:", balanceInfo.value.uiAmount);
+  console.log("Token balance in ui:", balanceInfo.value.uiAmount);
     console.log("Token balance:", balanceInfo.value.amount);
-    balanceInfoFee = balanceInfo.value.uiAmount; 
-    return  balanceInfo.value.uiAmount; // Restituisce l'importo del saldo;
+    balanceInfoFee = balanceInfo.value.uiAmount;     return  balanceInfo.value.amount; // Restituisce l'importo del saldo;
   } catch (error) {
     console.error("Errore nel controllo del saldo del token:", error);
     throw error; // Rilancia l'errore per la gestione
@@ -159,7 +164,7 @@ async function main() {
   console.log("Tokens Minted:", generateExplorerTxUrl(mintSig));
 
 
-  // Step 3 - Send Tokens from Owner to a New Account
+  // Step 3 - Send Tokens from Owner to a New 3 AccountS
   console.log("Step 3 - Send Tokens to 3 New Account:");
   for(let i=0; i < 3; i++){
       const destinationOwner = Keypair.generate();
@@ -216,77 +221,39 @@ async function main() {
   console.log("holders:", accountsToWithdrawFrom);
 
   
-  // Step 6 - Harvest Fees where I pass a wallet where I want to withdraw the fees
-  console.log("Step 6 - Harvest Fees");
-  //------------------------------------------------ WITHDRAW FEES ----------------------------------------------------------------------------------------------------
- 
 
-  const feeVault = Keypair.generate(); // ACCOUNT WHERE I DEPOSIT THE WHITDRAWED FEES 
-  // USE LOTTERY CONTRACT
-  console.log("nuovo account creato dove deposito i token", feeVault.publicKey);
-  // console.log("Keys: ", feeVault);
-  const feeVaultAccount = await createAssociatedTokenAccountIdempotent(
-    connection,
-    payer,
-    mint,
-    feeVault.publicKey,
-    {},
-    TOKEN_2022_PROGRAM_ID
-  );
+  //------------------------------------------------ WITHDRAW FEES ----------------------------------------------------------------------------------------------------
+   // Step 6 - Harvest Fees where I pass a wallet where I want to withdraw the fees
+   console.log("Step 6 - Harvest Fees");
+  
+   const destinationAccount = await getOrCreateAssociatedTokenAccount(
+     connection,
+     payer,
+     mint,
+     pda_lottery,
+     true,
+     "confirmed",
+     null,
+     TOKEN_2022_PROGRAM_ID,
+     ASSOCIATED_TOKEN_PROGRAM_ID
+   );
 
   const withdrawSig1 = await withdrawWithheldTokensFromAccounts(
     connection,
     payer,
     mint,
-    feeVaultAccount,
+    destinationAccount.address,
     withdrawWithheldAuthority,
     [],
     accountsToWithdrawFrom
   );
+
   console.log("Withdraw from Accounts:", generateExplorerTxUrl(withdrawSig1));
-
   
-  //------------------------------------------------ WITHDRAW FEES ----------------------------------------------------------------------------------------------------
-  
-  
-  // controllo quanti token sono nel wallet e li inserisco in una variabile
-  let balanceInfo = await checkTokenBalance(connection, feeVaultAccount) ;
-   const tokenToSend = 900000000000 / 1000000000;
-  const transferAmountToWithdraw = BigInt(balanceInfoFee * Math.pow(10, decimals)); // Transfer 1,000 tokens
-  console.log("trasferAmount 1_000: ",transferAmountToWithdraw);
+  console.log("Associed account: ", destinationAccount.address)
 
-  // Calculate the fee for the transfer
-  const calcFeeToWithdraw = (transferAmountToWithdraw * BigInt(feeBasisPoints)) / BigInt(10_000); // expect 10 fee
-  const feeToWithdraw = calcFeeToWithdraw > maxFee ? maxFee : calcFee; // expect 9 fee
+ // --------------------------------- CHANGE THE OWNERSHIP OF THE ASSOCIATED WALLET TO THE PROGRAM ID ------------------------------------------------------------
 
-
-/*
-  // trasferire i troken a un altro wallet -> lottery wallet C7zMLHuh7hYLZqrpwJ78SgZw4xFkRsSwEVdgtFQj4cWs
-  const lotteryWallet = Keypair.generate();
-  console.log("Keypair generated for the new Holder:", lotteryWallet );
-  console.log(lotteryWallet);
-  const destinationAccount = await createAssociatedTokenAccountIdempotent(
-    connection,
-    payer,
-    mint,
-    lotteryWallet.publicKey,
-    {},
-    TOKEN_2022_PROGRAM_ID
-  );
-  const transferSig = await transferCheckedWithFee(
-    connection,
-    payer,
-    sourceAccount,
-    mint,
-    destinationAccount,
-    owner,
-    transferAmountToWithdraw,
-    decimals,
-    fee,
-    []
-  );
-  console.log("Tokens Transfered:", generateExplorerTxUrl(transferSig));
-*/
 
   
 }
